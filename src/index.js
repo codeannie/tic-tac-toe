47ca400c2803b5b55d7c2cf83e5ce4183f60c18c
Annie Chiang <devbear22@gmail.com>
2017-12-19 14:56:24 -0800

    declare a winner

--- src/index.js
+++ src/index.js
@@ -23,7 +23,12 @@ class Board extends React.Component {
   handleClick(i) {
     //use slice to copy squares array prior to making changes 
     const squares = this.state.squares.slice();
-    squares[i] = 'x';
+    // ignore click if someone has won the game or if square is already filled
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? 'x' : 'o';
     this.setState({
       squares: squares,
       // flip the value of isNext when it's clicked so x & o can take turns
@@ -43,8 +48,13 @@ class Board extends React.Component {
   }
 
   render() {
-    // displays who is next
-    const status = 'Next Player: ' + (this.state.isNext ? 'x' : 'o');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+        status = 'Next Player: ' + (this.state.isNext ? 'x' : 'o');
+    }
 
     return (
       <div>
@@ -91,3 +101,24 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+// determine when the game is won - helper function
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
